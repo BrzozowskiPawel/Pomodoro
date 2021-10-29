c3e95e76d701815e02961375b07d5f1b58dbbc69
Paweł Brzozowski <mac@192.168.1.6>
2021-10-29 18:47:12 +0200

    added background video
    main screen now have the background video. Video could be different in final version, it need to be in .mp4 ext.

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playBackgroundVideo()
     }
 
-
+    func playBackgroundVideo() {
+        let path = Bundle.main.path(forResource: "backgroundVideo", ofType: ".mp4")
+        player =  AVPlayer(url: URL(fileURLWithPath: path!))
+        player?.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.view.layer.insertSublayer(playerLayer, at: 0)
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player!.currentItem)
+        player!.seek(to: CMTime.zero)
+        player!.play()
+        self.player?.isMuted = true
+    }
+    
+    @objc func playerItemDidReachEnd() {
+        player?.seek(to: CMTime.zero)
+    }
 }
 
